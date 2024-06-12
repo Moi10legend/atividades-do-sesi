0217c64d0d71a43554b427ef83595a8df5b13ca9
Moisés Freire <125685929+Moi10legend@users.noreply.github.com>
2024-06-12 10:32:47 -0300

    Add files via upload

--- Unidade 2/startup/js/login.js
+++ Unidade 2/startup/js/login.js
@@ -8,7 +8,7 @@ loginButton.addEventListener("click", doLogin)
 function doLogin(){
      if(((inputUser.value === "moisesflopesf@gmail.com") || (inputUser.value === "07136573200"))
      && (inputSenha.value === "senha098")){
-        window.location.href = "./paginaDeUsuario.html"
+        window.location.href = "./paginasDeUsuariosCadastrados/paginaDeUsuario.html"
      }else if((inputUser != "moisesflopesf@gmail.com") || (inputUser != "07136573200")){
       userErrorSpace.innerHTML = `<span class="userError">Usuário ou senha inválido</span>`
    }
