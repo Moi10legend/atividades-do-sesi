538664ea20fa55529a5a158f53629ffc8599020c
Moisés Freire <125685929+Moi10legend@users.noreply.github.com>
2024-08-28 08:09:55 -0300

    atualizando questão 8

--- unidade 3/exercicios objetos/questão 8.js
+++ unidade 3/exercicios objetos/questão 8.js
@@ -1,5 +1,5 @@
 class Cliente{
-    constructor(nome, cpf, dataDeNascimento, endereco, email, ocupacao, rendaAnual){
+    constructor(nome, cpf, dataDeNascimento, endereco, email, ocupacao, rendaAnual, nivelDeEscolaridade){
         this.nome = nome 
         this.cpf = cpf
         this.dataDeNascimento = dataDeNascimento
@@ -7,8 +7,58 @@ class Cliente{
         this.email = email
         this.ocupacao = ocupacao
         this.rendaAnual = rendaAnual
+        this.nivelDeEscolaridade = nivelDeEscolaridade
     }
 
+    get valorDoNome(){
+        return this.nome
+    }
+
+    set mudarNome(nome){
+        this.nome = nome
+    }
+
+    get valorDoCpf(){
+        return this.cpf
+    }
+
+    set mudarCpf(cpf){
+        this.cpf = cpf
+    }
+
+    get valorDaDataDenascimento(){
+        return this.dataDeNascimento
+    }
+
+    set novaDataDeNascimento(dataDeNascimento){
+        this.dataDeNascimento = dataDeNascimento
+    }
+
+    get valroDoEndereco(){
+        return this.endereco
+    }
+
+    set novoEndereço(endereco){
+        this.endereco = this.endereco
+    }
+
+    get valorDoEMail(){
+        return this.email
+    }
+
+    set novoEmail(email){
+        this.email = email
+    }
+
+    get valorDaOcupacao0(){
+        return this.ocupacao
+    }
+
+    set novaOcupacao(ocupacao){
+        this.ocupacao = ocupacao
+    }
+
+    get
     reatorioDoCliente(){
         console.log(`
         Nome: ${this.nome}
